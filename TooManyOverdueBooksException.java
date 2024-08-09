309777d5a188686d74007788a9006831cfa575e0
jerrysun2004 <89041024+jerrysun2004@users.noreply.github.com>
2024-08-08 17:55:41 -0700

    Update TooManyOverdueBooksException.java

--- TooManyOverdueBooksException.java
+++ TooManyOverdueBooksException.java
@@ -1,4 +1,8 @@
 public class TooManyOverdueBooksException extends Exception {
+    //this is the exception for users with too many overdue books
+    //have not implemented this feature into the application yet
+    //how to specify they are overdue
+    //does application keep track of time?
     public TooManyOverdueBooksException(String message) {
         super(message);
     }
