a36d40f60ce658370a031c92251b376562ea4943
jerrysun2004 <89041024+jerrysun2004@users.noreply.github.com>
2024-08-08 18:05:38 -0700

    Update BookNotAvailableException.java

--- BookNotAvailableException.java
+++ BookNotAvailableException.java
@@ -1,4 +1,5 @@
 public class BookNotAvailableException extends Exception {
+    //this custom exception will be used when the books is not avalable, another user has already checked out the book
     public BookNotAvailableException(String message) {
         super(message);
     }
